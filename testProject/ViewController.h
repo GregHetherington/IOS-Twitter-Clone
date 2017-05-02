b7e0cd61bf0bb170cdbe9f68b0aec019f1843e53
Greg <gregory@vouchr.co>
2017-05-02 15:42:18 -0400

    Initial Commit

---
+++ testProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testProject
+//
+//  Created by Greg Hetherington on 2017-05-02.
+//  Copyright © 2017 Greg Hetherington. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testProject/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  testProject
+//
+//  Created by Greg Hetherington on 2017-05-02.
+//  Copyright © 2017 Greg Hetherington. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
